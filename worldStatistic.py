6bb4cfee749ae7f460d95edafab7d76a7032778f
Thomas Bricknell <t.bricknell93@gmail.com>
2017-05-09 15:09:50 +1000

    Added class for handling a particular world statistic for a country/continent

---
+++ worldStatistic.py
@@ -0,0 +1,22 @@
+class WorldStatistic(object):
+    
+    def __init__(self, name, value, units):
+        self.name = name
+        self.value = value
+        self.units = units
+    
+    def getName(self):
+        return self.name
+    
+    def getValue(self):
+        return self.value
+    
+    def getUnits(self):
+        return self.units
+    
+    def __str__(self):
+        return self.name + ": " + str(self.value) + " " + self.units
+
+if __name__ == "__main__":
+    ws = WorldStatistic("TB Mortality", 40, "people/capita")
+    print(ws)
